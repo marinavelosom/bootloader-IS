bccd17f4dacd2422a3d984852cf1afdc77d76456
Wellinne <wwna@cin.ufpe.br>
2023-07-01 18:33:56 -0300

    enviando boneco

--- kernel.asm
+++ kernel.asm
@@ -27,11 +27,16 @@ data:
     palavra db "CASACO", 10, 0
     espacoPalavra db '------', 0
     fraseGanhou db "PARABENS, VOCE GANHOU", 10,0
+    frasePerdeu db "VOCE PERDEU", 10,0
     letra resb 2
     posLetra resb 2
     erro resb 2
     acerto resb 2
     i resb 1
+    boneco db '  O', 10, ' /|\', 10, '/ \', 0
+    posicao_bonecoX resb 2
+    posicao_bonecoY resb 2
+
 start:
     XOR ax, ax
     mov bx, ax
@@ -44,8 +49,9 @@ start:
     mov ah, 00h ;seta video mode
     mov al, 00h
     int 10h
+
 MostraInstrucao:
-    lea si, instrucao
+    mov si, instrucao
     PosicaoYX 10,3
     ;Imprime a instrucao: Digite uma leta
     LoopIntru:
@@ -57,16 +63,17 @@ MostraInstrucao:
         cmp al, 0
         jne LoopIntru
     call imprimeEspaco
-Compara:        
+
+Compara:
         mov dl, byte[espacoPalavra+bx]
         mov al, byte[palavra+bx]
         cmp al, dl
         jne LerLetra
-;**** Imprmindo só para testar        
+;**** Imprmindo só para testar
         mov ah, 0Eh ;imprime o que esta em al
         mov bh, 0
         int 10h
-        
+
         mov ah, 0Eh ;imprime o que esta em al
         mov bh, 0
         mov al, dl
@@ -77,9 +84,9 @@ Compara:
         mov bh, 0
         mov al, bl
         int 10h
-        
+
         ;isso fica
-        sub bl, '0' 
+        sub bl, '0'
         inc bx
         add bl, '0'
         ;----
@@ -87,7 +94,7 @@ Compara:
         mov bh, 0
         mov al, bl
         int 10h
-;************************* FIM TESTE        
+;************************* FIM TESTE
         cmp al, 0
         jne Compara
         jmp FimGanhou
@@ -106,7 +113,7 @@ LerLetra:
 ImprimePalavra:
     mov si, palavra
     xor cx, cx
-    
+
     PosicaoYX 12, 3
     LoopPalavra:
         lodsb
@@ -120,11 +127,15 @@ ImprimePalavra:
 
         cmp al, [letra]
         jne LoopPalavra
-        jmp LetraExiste 
-        
+        jmp LetraExiste
+
     Somar1 [erro]
-;;******imprmirir qtd erros       
-;        mov al, [erro] 
+    ;cmp [erro], 5
+    ;jmp FimPerdeu
+    ;jne ConstruirBoneco
+
+;;******imprmirir qtd erros
+;        mov al, [erro]
 ;        add al, '0'
 ;        mov ah, 0Eh ;imprime o que esta em al
 ;        mov bh, 0
@@ -138,14 +149,14 @@ LetraExiste: ;Aqui insere a [letra] na string de espaços, na posição [posLetr
     add bl, '0'
     sub bl, 1
     sub bl, '0'
-    mov byte[espacoPalavra+bx], dl ;coloca a letra na posição 
+    mov byte[espacoPalavra+bx], dl ;coloca a letra na posição
     Somar1 [acerto]
     jmp LoopPalavra
 imprimeEspaco:
     mov si, espacoPalavra
-    
+
     PosicaoYX 14, 3
-    
+
     LoopEspaco:
         lodsb
         mov ah, 0Eh ;imprime o que esta em al
@@ -164,7 +175,7 @@ FimGanhou:
     int 10h   ; Chamada de interrupção para limpar a tela
 
     mov si, fraseGanhou
-    
+
     LoopGanhou:
         lodsb
         mov ah, 0Eh ;imprime o que esta em al
@@ -173,12 +184,45 @@ FimGanhou:
         inc cx
         cmp al, 0
         jne LoopGanhou
-    
+
     jmp Fim
-    
+
 FimPerdeu:
+    PosicaoYX 10, 7
+
+    mov ah, 0 ; Função de limpar a tela
+    mov al, 3 ; Preenche a tela com o caractere de fundo
+    int 10h   ; Chamada de interrupção para limpar a tela
+
+    mov si, frasePerdeu
+
+    LoopPerdeu:
+        lodsb
+        mov ah, 0Eh ;imprime o que esta em al
+        mov bh, 0
+        int 10h
+        inc cx
+        cmp al, 0
+        jne LoopPerdeu
+
+    jmp Fim
 
 ConstruirForca:
+
 ConstruirBoneco:
-Fim:  
+    mov si, boneco 
+    
+    LoopBoneco:
+        lodsb
+        cmp al, 0
+        jne LoopBoneco
+        inc cx ;Incrementar o contador de linha e coluna
+        mov [posicao_bonecoX], cx
+        mov [posicao_bonecoY], cx
+        PosicaoYX [posicao_bonecoY], [posicao_bonecoX]
+        mov ah, 0Eh
+        mov bh, 0
+        int 10h
+
+Fim:
     jmp $
